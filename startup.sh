05075023ea318b5d1133876f157ebd580b2a49ea
Marcos <marcos@null.com>
2022-07-05 08:10:26 +0100

    update

--- startup.sh
+++ startup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 rm -rf ovpn_configs*
-curl -Ls ${SURFSHARK_CONFIG_URL} -o ovpn_configs.zip
+wget ${SURFSHARK_CONFIG_URL} -O ovpn_configs.zip
 unzip ovpn_configs.zip -d ovpn_configs
 cd ovpn_configs
 chinadir=China_*
